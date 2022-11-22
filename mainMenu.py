9e4191954fccd7985b81dfed363d771fd516e3b7
Elaine Liu <eliu72@uwo.ca>
2022-11-21 20:36:40 -0500

    Clean up mainMenu and add imports

--- mainMenu.py
+++ mainMenu.py
@@ -12,28 +12,11 @@ def main():
     print("Booking "+str(bookings[0].bookingId)+" is in space "+str(bookings[0].spaceId)+" and is held by "+bookings[0].userId)
 
 def login():
-
     user = User("chaines4@uwo.ca", isLibrarian=False)
     return
 
 def logout():
     return
-
-def bookSpace():
-    # datetime(year, month, day, hour, minute, second, microsecond)
-    startTime = datetime.datetime(2022, 11, 15, 14)
-    endTime = datetime.datetime(2022, 11, 15, 16)
-    newBooking = Booking(22, spaces[0].spaceId, "chaines4@uwo.ca", startTime, endTime)
-    bookings.append(newBooking)
-    print("Booking added")
-
-    return
-
-# def addSpace():
-#     newSpace = Space(123, 4, "filters", "Taylor Library 155")
-#     spaces.append(newSpace)
-#     print("Space added")
-#     return
 
 if __name__ == '__main__':
     main()
