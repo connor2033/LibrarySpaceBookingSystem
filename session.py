c8a5408165769b374928135b0e9e2fd75d9013a7
Elaine Liu <eliu72@uwo.ca>
2022-11-21 19:47:57 -0500

    Create Session class to store session-specific information

---
+++ session.py
@@ -0,0 +1,29 @@
+class Session:
+    def __init__(self, curr_user):
+        self.user = curr_user
+        self.loadBookings()
+        self.loadSpaces()
+    
+    def loadBookings():
+        pass
+
+    def loadSpaces():
+        pass
+
+    def saveBookings():
+        pass
+    
+    def saveSpaces():
+        pass
+    
+    def viewBookings():
+        pass
+    
+    def cancelBooking():
+        pass
+    
+    def addSpace():
+        pass
+    
+    def removeSpace():
+        pass
